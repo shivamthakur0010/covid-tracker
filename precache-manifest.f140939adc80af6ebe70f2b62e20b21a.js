0acf814ce621a49c61466384c62b2aa2f3959161
unknown <demonking0010@gmail.com>
2022-09-05 16:53:22 -0700

    Updates

---
+++ precache-manifest.f140939adc80af6ebe70f2b62e20b21a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50143872cbebdbee93917cdb6f681f38",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "0c1e25953a96c89ee936",
+    "url": "/covid-tracker/static/css/main.774f4958.chunk.css"
+  },
+  {
+    "revision": "c0ed790609bc208f4c1d",
+    "url": "/covid-tracker/static/js/2.b17163d8.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.b17163d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c1e25953a96c89ee936",
+    "url": "/covid-tracker/static/js/main.bf2691b9.chunk.js"
+  },
+  {
+    "revision": "ef7fedef9cd7db49b0f7",
+    "url": "/covid-tracker/static/js/runtime-main.adeb3781.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/image.d7265326.png"
+  }
+]);
